e05142edb0f3cc9f20843fcff64f5b1c566606c1
Andrew Shearer <andrewshearerdev@gmail.com>
2023-09-25 16:36:08 -0700

    added db config and created 2 library functions for querying the db

---
+++ src/lib/getAllQuotes.ts
@@ -0,0 +1,34 @@
+import { drizzle } from "drizzle-orm/planetscale-serverless";
+import { eq } from "drizzle-orm";
+import { connect } from "@planetscale/database";
+
+import { config } from "@/db/config";
+import { quotes, authors, categories } from "@/db/schema";
+import type { Quote } from "@/types";
+
+const getAllQuotes = async (): Promise<Quote[]> => {
+  // connect to the database
+  const connection = connect(config);
+  const database = drizzle(connection);
+
+  // query the database with drizzle
+  const results: Quote[] = await database
+    .select({
+      author: authors.author,
+      category: categories.category,
+      quote: quotes.quote // we want a quote: from quotesTable.quotesColumn
+    })
+    .from(quotes) // from the quotes table
+    .innerJoin(authors, eq(quotes.authorId, authors.id))
+    .innerJoin(categories, eq(quotes.categoryId, categories.id));
+
+  // raw SQL query for comparison:
+  // SELECT A.author, C.category, Q.quote
+  // FROM quotes Q
+  // INNER JOIN authors A ON Q.author_id = A.id
+  // INNER JOIN categories C on Q.category_id = C.id;
+
+  return results;
+};
+
+export default getAllQuotes;
---
+++ src/lib/getRandomQuote.ts
@@ -0,0 +1,27 @@
+import getAllQuotes from "./getAllQuotes";
+import type { Quote } from "@/types";
+
+// keep track of some state outside the function
+// this avoids returning the same quote we had before
+const previousQuoteState = {
+  previous: 1,
+  setPrevious: function (num: number) {
+    this.previous = num;
+  }
+};
+
+const getRandomQuote = async (): Promise<Quote> => {
+  const results = await getAllQuotes();
+
+  let randomIndex = previousQuoteState.previous;
+
+  while (randomIndex === previousQuoteState.previous) {
+    randomIndex = Math.floor(Math.random() * results.length);
+  }
+
+  previousQuoteState.setPrevious(randomIndex);
+
+  return results[randomIndex];
+};
+
+export default getRandomQuote;
